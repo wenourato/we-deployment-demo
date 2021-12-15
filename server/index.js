e7425d21221b0f76c034c7a0635b7e4ce34660e5
Wyatt Enourato <wenourato@gmail.com>
2021-12-15 11:36:24 -0700

    updated

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const res = require('express/lib/response')
+// const res = require('express/lib/response')
 const path = require('path')
 
 const app = express()
